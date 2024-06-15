b9dd53dc2d225461f217a0a6d39cbe1e04a7dd08
jcochran <jcochran@enova.com>
2024-06-14 21:08:42 -0500

    steering with moving clouds and sky

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -10,21 +9,24 @@ import (
 )
 
 // Game implements ebiten.Game interface.
-type Game struct{}
+type Game struct {
+	counter int
+}
 
 var tilt int
+var roadCount = 1
 
 // Update proceeds the game state.
 // Update is called every tick (1/60 [s] by default).
 func (g *Game) Update() error {
 	// Write your game's logical update.
-	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		if tilt > -3 {
+	if ebiten.IsKeyPressed(ebiten.KeyA) || ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		if tilt > -4 {
 			tilt--
 		}
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyD) {
-		if tilt < 3 {
+	if ebiten.IsKeyPressed(ebiten.KeyD) || ebiten.IsKeyPressed(ebiten.KeyRight) {
+		if tilt < 4 {
 			tilt++
 		}
 	}
@@ -34,55 +36,59 @@ func (g *Game) Update() error {
 // Draw draws the game screen.
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
-	// Write your game's rendering.
-	bgImage := ebiten.NewImage(640, 480)
-	bgImage.Fill(color.RGBA{0x80, 0xa0, 0xc0, 0xff})
 	var eimg *ebiten.Image
 	switch tilt {
+	case -4:
+		img, _, err := ebitenutil.NewImageFromFile("./left_crash.png")
+		if err != nil {
+			log.Fatal(err)
+		}
+		eimg = img
+	case 4:
+		img, _, err := ebitenutil.NewImageFromFile("./right_crash.png")
+		if err != nil {
+			log.Fatal(err)
+		}
+		eimg = img
 	case -3:
-		// load image
 		img, _, err := ebitenutil.NewImageFromFile("./left3.png")
 		if err != nil {
 			log.Fatal(err)
 		}
 		eimg = img
 	case -2:
-		// load image
 		img, _, err := ebitenutil.NewImageFromFile("./left2.png")
 		if err != nil {
 			log.Fatal(err)
 		}
 		eimg = img
 	case -1:
-		// load image
 		img, _, err := ebitenutil.NewImageFromFile("./left1.png")
 		if err != nil {
 			log.Fatal(err)
 		}
 		eimg = img
 	case 0:
-		// load image
-		img, _, err := ebitenutil.NewImageFromFile("./straight (1).png")
+		img, _, err := ebitenutil.NewImageFromFile("./straight.png")
 		if err != nil {
 			log.Fatal("what", err)
 		}
 		eimg = img
 	case 1:
-		// load image
 		img, _, err := ebitenutil.NewImageFromFile("./right1.png")
 		if err != nil {
 			log.Fatal(err)
 		}
 		eimg = img
 	case 2:
-		// load image
+
 		img, _, err := ebitenutil.NewImageFromFile("./right2.png")
 		if err != nil {
 			log.Fatal(err)
 		}
 		eimg = img
 	case 3:
-		// load image
+
 		img, _, err := ebitenutil.NewImageFromFile("./right3.png")
 		if err != nil {
 			log.Fatal(err)
@@ -90,12 +96,45 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		eimg = img
 	}
 
-	op := &ebiten.DrawImageOptions{}
-
 	//dRAW IMAGE
-	fmt.Println(tilt)
-	screen.DrawImage(bgImage, op)
+	opts := &ebiten.DrawImageOptions{}
+	opts.GeoM.Scale(0.5, 1)
+	opts.GeoM.Translate(0, 175)
+
+	var road string
+	var sky string
+	g.counter++
+	if g.counter%30 == 0 {
+		if roadCount == 3 {
+			roadCount = 1
+		} else {
+			roadCount++
+		}
+		road = fmt.Sprintf("./theRoad%d.png", roadCount)
+		sky = fmt.Sprintf("./theSky%d.png", roadCount)
+	} else {
+		road = fmt.Sprintf("./theRoad%d.png", roadCount)
+		sky = fmt.Sprintf("./theSky%d.png", roadCount)
+	}
+
+	img, _, err := ebitenutil.NewImageFromFile(sky)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	skyopts := &ebiten.DrawImageOptions{}
+	skyopts.GeoM.Scale(.5, 1)
+	screen.DrawImage(img, skyopts)
+
+	img, _, err = ebitenutil.NewImageFromFile(road)
+	if err != nil {
+		log.Fatal(err)
+	}
+	screen.DrawImage(img, opts)
+
 	if eimg != nil {
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Translate(0, 130)
 		screen.DrawImage(eimg, op)
 	}
 }
@@ -103,14 +142,15 @@ func (g *Game) Draw(screen *ebiten.Image) {
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 // If you don't have to adjust the screen size with the outside size, just return a fixed size.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return 320, 450
 }
 
 func main() {
 	game := &Game{}
 	// Specify the window size as you like. Here, a doubled size is specified.
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Your game's title")
+	ebiten.SetWindowSize(640, 900)
+	ebiten.Monitor().Size()
+	ebiten.SetWindowTitle("Seeking Tokyo Finding Fuji")
 	// Call ebiten.RunGame to start your game loop.
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
