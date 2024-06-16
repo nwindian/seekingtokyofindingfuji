b1819d1c03aa7475bba4d52317a5c50568e80102
Joshua Foss <jmfoss98@gmail.com>
2024-06-16 15:00:41 -0500

    adjust balancing based off speed and add reset message after a crash (#4)
    * Adjust balancing based on speed and include restart png
    
    * Fix Reset Rendering and balance balance
    
    * Add title screen
    
    * Add sideBike sprite for the fuji jump
    
    * Add fujiSide
    
    * Add transition to fuji after reaching the distance

--- main.go
+++ main.go
@@ -14,13 +14,20 @@ type Game struct {
 	counter int
 }
 
+const (
+	screenWidth  = 640
+	screenHeight = 900
+)
+
 var tilt int
 var roadCount = 1
 var skyCount = 1
-var crash = false
+var crashed = false
+var crashedFade = 0
+var started = false
 
 // Used for distance
-const FUJI_DISTANCE = 10000
+const FUJI_DISTANCE = 100
 
 var currentDistance = 0.0
 
@@ -76,193 +83,269 @@ func UpdateBPS2() {
 }
 
 func UpdateBalance() {
-	if ticksSinceLastPress%30 == 0 {
+	tiltRate := 60 - min(55, int(bananasPerSecond)/3)
+
+	if ticksSinceLastPress%tiltRate == 0 {
 		if tilt < 0 {
-			tilt--
+			tilt -= 1
 		} else if tilt > 0 {
-			tilt++
+			tilt += 1
 		}
 	}
+}
+
+func ShowReset() {
+
 }
 
 // Update proceeds the game state.
 // Update is called every tick (1/60 [s] by default).
 func (g *Game) Update() error {
 	// Write your game's logical update.
-	leftPressed := inpututil.IsKeyJustPressed(ebiten.KeyA) || inpututil.IsKeyJustPressed(ebiten.KeyLeft)
-	rightPressed := inpututil.IsKeyJustPressed(ebiten.KeyD) || inpututil.IsKeyJustPressed(ebiten.KeyRight)
-	straightPressed := inpututil.IsKeyJustPressed(ebiten.KeyW) || inpututil.IsKeyJustPressed(ebiten.KeyUp)
+	if !crashed && started {
+		leftPressed := inpututil.IsKeyJustPressed(ebiten.KeyA) || inpututil.IsKeyJustPressed(ebiten.KeyLeft)
+		rightPressed := inpututil.IsKeyJustPressed(ebiten.KeyD) || inpututil.IsKeyJustPressed(ebiten.KeyRight)
+		straightPressed := inpututil.IsKeyJustPressed(ebiten.KeyW) || inpututil.IsKeyJustPressed(ebiten.KeyUp)
 
-	if leftPressed {
-		if tilt == 1 {
-			tilt = -1
+		if leftPressed {
+			if tilt == 1 {
+				tilt = -1
+			} else {
+				tilt--
+			}
+		}
+		if rightPressed {
+			if tilt == -1 {
+				tilt = 1
+			} else {
+				tilt++
+			}
+		}
+		if straightPressed {
+			if tilt == -1 || tilt == 1 {
+				tilt = 0
+			}
+		}
+
+		if leftPressed && rightPressed {
+			Crash()
+		} else if leftPressed != rightPressed {
+			UpdateAcceleration()
+			ticksSinceLastPress = 0
+
 		} else {
-			tilt--
+			UpdateBalance()
 		}
-	}
-	if rightPressed {
-		if tilt == -1 {
-			tilt = 1
-		} else {
-			tilt++
+
+		if tilt == -4 || tilt == 4 {
+			Crash()
 		}
-	}
-	if straightPressed {
-		if tilt == -1 || tilt == 1 {
-			tilt = 0
+
+		UpdateSpeed()
+		ticksSinceLastPress++
+		if ticksSinceLastPress > 60 {
+			Decelerate()
 		}
+
+		UpdateBPS()
+		UpdateBPS2()
+		UpdateDistance()
+	} else if started {
+		crashedFade++
+		if inpututil.IsKeyJustPressed(ebiten.KeyR) && crashedFade >= 60 {
+			reset()
+		}
+	} else if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
+		started = true
 	}
-
-	if leftPressed && rightPressed {
-		// Straight crash
-		crash = true
-	} else if leftPressed != rightPressed {
-		UpdateAcceleration()
-		ticksSinceLastPress = 0
-
-	} else {
-		UpdateBalance()
-	}
-
-	if tilt == -3 || tilt == 3 {
-		// end
-		fmt.Print("Game Over\n")
-	}
-
-	UpdateSpeed()
-	ticksSinceLastPress++
-	if ticksSinceLastPress > 60 {
-		Decelerate()
-	}
-
-	UpdateBPS()
-	UpdateBPS2()
-	UpdateDistance()
 
 	return nil
+}
+
+func reset() {
+	currentDistance = 0
+	tilt = 0
+	crashedFade = 0
+	crashed = false
+}
+
+func Crash() {
+	speed = 0
+	acceleration = 0
+	crashed = true
 }
 
 // Draw draws the game screen.
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
 	var eimg *ebiten.Image
-	if crash {
-		img, _, err := ebitenutil.NewImageFromFile("./straight_crash.png")
+	var middleText *ebiten.Image
+	var titleScreen *ebiten.Image
+
+	if currentDistance < FUJI_DISTANCE {
+		if crashed {
+			switch tilt {
+			case -4:
+				img, _, err := ebitenutil.NewImageFromFile("./left_crash.png")
+				if err != nil {
+					log.Fatal(err)
+				}
+				eimg = img
+			case 4:
+				img, _, err := ebitenutil.NewImageFromFile("./right_crash.png")
+				if err != nil {
+					log.Fatal(err)
+				}
+				eimg = img
+			default:
+				img, _, err := ebitenutil.NewImageFromFile("./straight_crash.png")
+				if err != nil {
+					log.Fatal(err)
+				}
+				eimg = img
+			}
+			if crashedFade >= 60 {
+				eimg = nil
+				if g.counter%60 < 30 {
+					img, _, err := ebitenutil.NewImageFromFile("./restart.png")
+					if err != nil {
+						log.Fatal(err)
+					}
+					middleText = img
+				}
+			}
+		} else if started {
+			switch tilt {
+			case -3:
+				img, _, err := ebitenutil.NewImageFromFile("./left3.png")
+				if err != nil {
+					log.Fatal(err)
+				}
+				eimg = img
+			case -2:
+				img, _, err := ebitenutil.NewImageFromFile("./left2.png")
+				if err != nil {
+					log.Fatal(err)
+				}
+				eimg = img
+			case -1:
+				img, _, err := ebitenutil.NewImageFromFile("./left1.png")
+				if err != nil {
+					log.Fatal(err)
+				}
+				eimg = img
+			case 0:
+				img, _, err := ebitenutil.NewImageFromFile("./straight.png")
+				if err != nil {
+					log.Fatal("what", err)
+				}
+				eimg = img
+			case 1:
+				img, _, err := ebitenutil.NewImageFromFile("./right1.png")
+				if err != nil {
+					log.Fatal(err)
+				}
+				eimg = img
+			case 2:
+				img, _, err := ebitenutil.NewImageFromFile("./right2.png")
+				if err != nil {
+					log.Fatal(err)
+				}
+				eimg = img
+			case 3:
+
+				img, _, err := ebitenutil.NewImageFromFile("./right3.png")
+				if err != nil {
+					log.Fatal(err)
+				}
+				eimg = img
+			}
+		} else {
+			img, _, err := ebitenutil.NewImageFromFile("./title.png")
+			if err != nil {
+				log.Fatal(err)
+			}
+			titleScreen = img
+		}
+
+		//dRAW IMAGE
+		opts := &ebiten.DrawImageOptions{}
+		opts.GeoM.Scale(0.5, 1)
+		opts.GeoM.Translate(0, 175)
+
+		var road string
+		var sky string
+
+		roadRate := 60 - min(59, int(bananasPerSecond)/2)
+
+		g.counter++
+		if speed != 0 && g.counter%roadRate == 0 {
+			if roadCount == 3 {
+				roadCount = 1
+			} else {
+				roadCount++
+			}
+		}
+
+		if g.counter%30 == 0 {
+			if skyCount == 3 {
+				skyCount = 1
+			} else {
+				skyCount++
+			}
+		}
+
+		road = fmt.Sprintf("./theRoad%d.png", roadCount)
+		sky = fmt.Sprintf("./theSky%d.png", skyCount)
+
+		img, _, err := ebitenutil.NewImageFromFile(sky)
 		if err != nil {
 			log.Fatal(err)
 		}
-		eimg = img
+
+		skyopts := &ebiten.DrawImageOptions{}
+		skyopts.GeoM.Scale(.5, 1)
+		screen.DrawImage(img, skyopts)
+
+		img, _, err = ebitenutil.NewImageFromFile(road)
+		if err != nil {
+			log.Fatal(err)
+		}
+		screen.DrawImage(img, opts)
+
+		if eimg != nil {
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(0, 130)
+			screen.DrawImage(eimg, op)
+		}
+
+		if middleText != nil {
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(0, 150)
+			op.GeoM.Scale(.25, .25)
+			screen.DrawImage(middleText, op)
+		}
+
+		if titleScreen != nil {
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(0, 50)
+			op.GeoM.Scale(.25, .25)
+			screen.DrawImage(titleScreen, op)
+		}
+		ebitenutil.DebugPrint(screen, fmt.Sprintf("Speed: %.2f Bananas / Sec\nAcceleration: %.2f Bananas / Sec^2\nDistance: %.2f Bananas\nCompleted: %.1f%%\n", bananasPerSecond, bananasPerSecond2, currentDistance, (currentDistance*100)/FUJI_DISTANCE))
 	} else {
-		switch tilt {
-		case -4:
-			img, _, err := ebitenutil.NewImageFromFile("./left_crash.png")
-			if err != nil {
-				log.Fatal(err)
-			}
-			eimg = img
-		case 4:
-			img, _, err := ebitenutil.NewImageFromFile("./right_crash.png")
-			if err != nil {
-				log.Fatal(err)
-			}
-			eimg = img
-		case -3:
-			img, _, err := ebitenutil.NewImageFromFile("./left3.png")
-			if err != nil {
-				log.Fatal(err)
-			}
-			eimg = img
-		case -2:
-			img, _, err := ebitenutil.NewImageFromFile("./left2.png")
-			if err != nil {
-				log.Fatal(err)
-			}
-			eimg = img
-		case -1:
-			img, _, err := ebitenutil.NewImageFromFile("./left1.png")
-			if err != nil {
-				log.Fatal(err)
-			}
-			eimg = img
-		case 0:
-			img, _, err := ebitenutil.NewImageFromFile("./straight.png")
-			if err != nil {
-				log.Fatal("what", err)
-			}
-			eimg = img
-		case 1:
-			img, _, err := ebitenutil.NewImageFromFile("./right1.png")
-			if err != nil {
-				log.Fatal(err)
-			}
-			eimg = img
-		case 2:
-			img, _, err := ebitenutil.NewImageFromFile("./right2.png")
-			if err != nil {
-				log.Fatal(err)
-			}
-			eimg = img
-		case 3:
+		fuji, _, err := ebitenutil.NewImageFromFile("./fujiSide.png")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-			img, _, err := ebitenutil.NewImageFromFile("./right3.png")
-			if err != nil {
-				log.Fatal(err)
-			}
-			eimg = img
+		if fuji != nil {
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(0, -50)
+			op.GeoM.Scale(.25, .25)
+			screen.DrawImage(fuji, op)
 		}
 	}
-
-	//dRAW IMAGE
-	opts := &ebiten.DrawImageOptions{}
-	opts.GeoM.Scale(0.5, 1)
-	opts.GeoM.Translate(0, 175)
-
-	var road string
-	var sky string
-
-	roadRate := 60 - min(59, int(bananasPerSecond)/2)
-
-	g.counter++
-	if speed != 0 && g.counter%roadRate == 0 {
-		if roadCount == 3 {
-			roadCount = 1
-		} else {
-			roadCount++
-		}
-	}
-
-	if g.counter%30 == 0 {
-		if skyCount == 3 {
-			skyCount = 1
-		} else {
-			skyCount++
-		}
-	}
-
-	road = fmt.Sprintf("./theRoad%d.png", roadCount)
-	sky = fmt.Sprintf("./theSky%d.png", skyCount)
-
-	img, _, err := ebitenutil.NewImageFromFile(sky)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	skyopts := &ebiten.DrawImageOptions{}
-	skyopts.GeoM.Scale(.5, 1)
-	screen.DrawImage(img, skyopts)
-
-	img, _, err = ebitenutil.NewImageFromFile(road)
-	if err != nil {
-		log.Fatal(err)
-	}
-	screen.DrawImage(img, opts)
-
-	if eimg != nil {
-		op := &ebiten.DrawImageOptions{}
-		op.GeoM.Translate(0, 130)
-		screen.DrawImage(eimg, op)
-	}
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("Speed: %.2f Bananas / Sec\nAcceleration: %.2f Bananas / Sec^2\nDistance: %.2f Bananas\nCompleted: %.1f%%\n", bananasPerSecond, bananasPerSecond2, currentDistance, (currentDistance*100)/FUJI_DISTANCE))
 }
 
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
@@ -274,7 +357,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	game := &Game{}
 	// Specify the window size as you like. Here, a doubled size is specified.
-	ebiten.SetWindowSize(640, 900)
+	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.Monitor().Size()
 	ebiten.SetWindowTitle("Seeking Tokyo Finding Fuji")
 	// Call ebiten.RunGame to start your game loop.
